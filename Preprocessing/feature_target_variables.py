3bfb7238dde41c078ec236e37e444600ab12b351
Paolo <paolohardolino@gmail.com>
2025-12-23 12:27:08 +0100

    primi test , fix della probabilità nel knn e nella roc e auc, utilizzo di os generico per le feature_target_var

--- Preprocessing/feature_target_variables.py
+++ Preprocessing/feature_target_variables.py
@@ -1,7 +1,15 @@
 import pandas as pd
+import os
+
+# Ottiene il percorso della directory padre di questo file
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# risalgo alla cartella principale del progetto
+project_root = os.path.dirname(current_dir)
+os.chdir(project_root)
+cleaned_file_path = os.path.join(project_root, 'version_1_cleaned.csv')
 
 # Carica il dataset pulito
-df_clean = pd.read_csv('version_1_cleaned.csv')
+df_clean = pd.read_csv(cleaned_file_path)
 
 # Definisce la variabile target
 target_col = 'classtype_v1'
